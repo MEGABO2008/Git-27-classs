947a4f69cf3a033a26e50ea5c44c977a8bbac272
Bryden <o1101329@students.katyisd.org>
2020-12-19 16:40:40 -0600

    create a chain!

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint;
+var log6;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var chain;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -23,7 +24,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+    
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -33,9 +34,9 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 = new Log(230,180,80,PI/2);
     bird = new Bird(100,100);
-
+    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -54,11 +55,12 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
-
+    log6.display();
     box5.display();
     log4.display();
     log5.display();
-
     bird.display();
     platform.display();
+
+    chain.display();
 }
